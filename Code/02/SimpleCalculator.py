2c4f9d298e7e51dcd5ed6f2c09976f53b13a3ee4
Iman <imankhanijazani@gmail.com>
2023-02-21 05:52:43 +0330

    added simple calculator

---
+++ Code/02/SimpleCalculator.py
@@ -0,0 +1,13 @@
+
+meghdar_yek = 3
+meghdar_do = 78.1
+
+natije_jam = meghdar_yek + meghdar_do
+
+natije_menha = meghdar_yek - meghdar_do
+
+natije_zarb = meghdar_yek * meghdar_do
+
+print(natije_jam)
+print(natije_menha)
+print(natije_zarb)
