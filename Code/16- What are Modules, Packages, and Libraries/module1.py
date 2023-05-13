0a92ced7c4ff97d4c4aebf0f7db3fabed2c650e5
Iman <imankhanijazani@gmail.com>
2023-05-13 17:13:51 +0430

    minor update

---
+++ Code/16- What are Modules, Packages, and Libraries/module1.py
@@ -0,0 +1,6 @@
+a = 3
+def my_function_1(num):
+    return num**2
+
+def my_function_2(num):
+    return num*4
---
+++ Code/16- What are Modules, Packages, and Libraries/module2.py
@@ -0,0 +1,5 @@
+from module1 import *
+
+print(my_function_2(3))
+print(my_function_1(3))
+print(a)
